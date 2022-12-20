d6dc74a40c2a9cab157424a2f3b7e509a9709983
ho-229 <56023510+ho-229@users.noreply.github.com>
2022-12-20 20:27:12 +0800

    feat: beta

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,5 +1,20 @@
 r18::init!("tr");
 
 fn main() {
-    println!("Hello, world!");
+    let name = "ho-229";
+    r18::set_locale!("");
+    assert_eq!(format!("Hello, {}", name), r18::tr!("Hello, {}", name));
+
+    r18::set_locale!("zh_CN");
+    assert_eq!(format!("你好，{}", name), r18::tr!("Hello, {}", name));
+    assert_eq!(
+        r#"调试：Custom { kind: Other, error: "An error" }"#,
+        r18::tr!(
+            "Debug: {}",
+            format!(
+                "{:?}",
+                std::io::Error::new(std::io::ErrorKind::Other, "An error")
+            )
+        )
+    );
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::{collections::HashMap, sync::Mutex};
 
 pub use once_cell::sync::Lazy;
+pub use dynfmt::{Format, SimpleCurlyFormat};
 
 pub use proc_macros::init;
 
@@ -12,6 +13,20 @@ pub struct Locale {
 pub static CURRENT_LOCALE: Lazy<Mutex<Option<&'static Lazy<Locale>>>> =
     Lazy::new(|| Mutex::new(None));
 
-pub fn translate(prefix: &str, content: &str) {}
+pub fn translate<'a>(prefix: impl AsRef<str>, content: &'a str) -> &'a str {
+    let locale = CURRENT_LOCALE.lock().unwrap();
+    let locale = match *locale {
+        Some(l) => l,
+        None => return content,
+    };
+
+    match locale
+        .translate
+        .get(format!("{}.{}", prefix.as_ref(), content).as_str())
+    {
+        Some(tr) => tr,
+        None => content,
+    }
+}
 
 mod_use::mod_use!(macros);
