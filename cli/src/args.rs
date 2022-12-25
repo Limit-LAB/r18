2091416557e930a9afaab4f2590715e7864ac6a6
ho-229 <56023510+ho-229@users.noreply.github.com>
2022-12-25 22:07:21 +0800

    feat: CLI support [WIP] [skip-ci]

--- cli/src/args.rs
+++ cli/src/args.rs
@@ -1,10 +1,25 @@
 pub(crate) use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
-#[command(author, version, about)]
+#[command(author, version, about, bin_name = "cargo")]
 pub(crate) struct Args {
     #[command(subcommand)]
-    command: Command,
+    command: SelfCommand,
+}
+
+#[derive(Parser)]
+pub(crate) struct InnerArgs {
+    #[command(subcommand)]
+    pub command: Command,
+
+    /// Root directory of your project
+    #[arg(short, long, default_value_t = { "./".to_string() })]
+    pub root: String,
+}
+
+#[derive(Subcommand)]
+enum SelfCommand {
+    R18(InnerArgs)
 }
 
 #[derive(Subcommand)]
@@ -14,5 +29,12 @@ pub(crate) enum Command {
     /// Generate translation files named with language tag
     Generate {
         locale: Vec<String>,
+    }
+}
+
+impl Args {
+    pub fn inner_args(self) -> InnerArgs {
+        let SelfCommand::R18(args) = self.command;
+        args
     }
 }
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,5 +1,79 @@
+use std::{collections::HashSet, path::Path};
+
+use walkdir::WalkDir;
+
 mod_use::mod_use!(args);
 
+pub(crate) type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
 fn main() {
-    let args = args::Args::parse();
+    let args = args::Args::parse().inner_args();
+
+    if let Err(e) = match args.command {
+        Command::Update => {
+            if Path::new(&args.root).is_dir() {
+                update(&args.root)
+            } else {
+                Err("Invalid project root".into())
+            }
+        }
+        Command::Generate { locale } => todo!(),
+    } {
+        eprintln!("Error: {}", e);
+    }
+}
+
+fn update(root: impl AsRef<Path>) -> Result<()> {
+    let mut contents = HashSet::new();
+    let mut locale = String::new();
+
+    for entry in WalkDir::new(root.as_ref().join("src"))
+        .into_iter()
+        .filter_map(|entry| {
+            entry.ok().and_then(|entry| {
+                entry
+                    .path()
+                    .extension()
+                    .map(|ext| ext == "rs")
+                    .unwrap_or(false)
+                    .then_some(entry)
+            })
+        })
+    {
+        r18_trans_support::source::extract(entry.path(), &mut contents, &mut locale)?;
+    }
+
+    if locale.is_empty() {
+        return Err("Missing translation directory".into());
+    }
+
+    for entry in WalkDir::new(root.as_ref().join(locale))
+        .into_iter()
+        .filter_map(|entry| {
+            entry.ok().and_then(|entry| {
+                entry
+                    .path()
+                    .extension()
+                    .map(|ext| ext == "rs")
+                    .unwrap_or(false)
+                    .then_some(entry)
+            })
+        })
+    {
+        let mut is_modified = false;
+        let mut translation = r18_trans_support::translation::extract(entry.path());
+
+        for content in contents.iter() {
+            if !translation.contains_key(content) {
+                is_modified = true;
+                translation.insert(content.into(), "[TODO]".into());
+            }
+        }
+
+        if is_modified {
+
+        }
+    }
+
+    Ok(())
 }
